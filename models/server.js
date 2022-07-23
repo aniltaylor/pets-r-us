cda607f0852dd185c1d525d5d94aca51892335e7
Anita L Taylor <98141576+aniltaylor@users.noreply.github.com>
2022-07-22 21:47:31 -0500

    Add files via upload

---
+++ models/server.js
@@ -0,0 +1,177 @@
+
+// get user list
+var express = require('express');
+var app = express();
+var fs = require("fs");
+
+app.get('/listUsers', function (req, res) {
+   fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
+      console.log( data );
+      res.end( data );
+   });
+})
+
+var server = app.listen(3000, function () {
+   var host = server.address().address
+   var port = server.address().port
+   console.log("Example app listening at http://%s:%s", host, port)
+})
+
+// add user to list (put)
+var express = require('express');
+var app = express();
+var fs = require("fs");
+
+var user = {
+   "user4" : {
+      "name" : "mohit",
+      "email" : "mohit@gmail.com",
+      "appointment" : " August 4",
+      "id": 4
+   }
+}
+app.post('/addUser', function (req, res) {
+   // First read existing users.
+   fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
+      data = JSON.parse( data );
+      data["user4"] = user["user4"];
+      console.log( data );
+      res.end( JSON.stringify(data));
+   });
+})
+var server = app.listen(3000, function () {
+   var host = server.address().address
+   var port = server.address().port
+   console.log("Example app listening at http://%s:%s", host, port)
+})
+
+// Show detail of users
+var express = require('express');
+var app = express();
+var fs = require("fs");
+
+app.get('/:id', function (req, res) {
+   // First read existing users.
+   fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
+      var users = JSON.parse( data );
+      var user = users["user" + req.params.id] 
+      console.log( user );
+      res.end( JSON.stringify(user));
+   });
+})
+var server = app.listen(3000, function () {
+   var host = server.address().address
+   var port = server.address().port
+   console.log("Example app listening at http://%s:%s", host, port)
+})
+
+// delete user profile
+var express = require('express');
+var app = express();
+var fs = require("fs");
+
+var id = 2;
+
+app.delete('/deleteUser', function (req, res) {
+   // First read existing users.
+   fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
+      data = JSON.parse( data );
+      delete data["user" + 2];
+       
+      console.log( data );
+      res.end( JSON.stringify(data));
+   });
+})
+
+var server = app.listen(8081, function () {
+   var host = server.address().address
+   var port = server.address().port
+   console.log("Example app listening at http://%s:%s", host, port)
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ models/users.js
@@ -0,0 +1,22 @@
+{
+   "user1" : {
+      "name" : "mahesh",
+      "email" : "mahesh@gmail.com",
+      "appointment" : "August 5",
+      "id": 1
+   },
+   
+   "user2" : {
+      "name" : "suresh",
+      "email" : "suresh@gmail.com",
+      "appointment" : "August 2",
+      "id": 2
+   },
+   
+   "user3" : {
+      "name" : "ramesh",
+      "email" : "August 2",
+      "appointment" : "clerk",
+      "id": 3
+   }
+}
